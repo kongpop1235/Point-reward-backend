12223da911d61bc32a3655cf90906e29b069246c
Kongpop(Kai) <ru.kongpop@hotmail.com>
2024-12-09 13:32:45 +0700

    [Edit] Edit required in AdvertisedProduct.js
    -Edit title required : false
    -Edit description: false

--- models/AdvertisedProduct.js
+++ models/AdvertisedProduct.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const advertisedProductSchema = new mongoose.Schema({
   imageUrl: { type: String, required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: false },
+  description: { type: String, required: false },
   productId: { type: String, required: true }
 });
 
