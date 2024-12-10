1005790b6ab5c0436b9c40f8f340f10c5bef19ff
Kongpop(Kai) <kongpop.r@ragnar.co.th>
2024-12-10 17:52:51 +0700

    [FIX] Fix Docker

--- app.js
+++ app.js
@@ -16,9 +16,12 @@ app.use(cors({
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
